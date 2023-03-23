008b9891aebfa94ff3d38088251d7d49d8c85615
chanakya ram sai illa <chanakyaramsaiilla@gmail.com>
2023-03-23 11:04:01 +0530

    checking in initial main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from customtkinter import *
+from tkinter import *
+from tkinter import messagebox as msgb
+from win32api import GetSystemMetrics as GSM
+import keyboard
+
+def confirm_close():
+   ans = msgb.askyesno(title="close", message="are you sure you want to close?", icon="warning")
+   if ans:
+    mainwindow.destroy()
+mainwindow = CTk()
+editor = Text(height=GSM(0), width=GSM(1))
+editor.pack()
+mainwindow.protocol('WM_DELETE_WINDOW', confirm_close)
+mainwindow.title('PDF-NET')
+
+mainwindow.mainloop()
